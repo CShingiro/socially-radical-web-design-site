1418350e37baad98eaaf38e0f2dc971cec40d118
CShingiro <sociallyradicalwebdesign@gmail.com>
2024-09-07 13:01:07 +0700

    New Fixes to web design site

--- assets/vue-i18n.runtime.esm-bundler.affae9b9.js
+++ assets/vue-i18n.runtime.esm-bundler.affae9b9.js
@@ -1,4 +1,4 @@
-import{h as Xe,e as wt,i as Wt,o as Ut,a as Vt,r as Q,c as q,w as ce,F as xt,g as Ye,d as jt,f as Gt,T as $t}from"./index.ef04a33c.js";/*!
+import{h as Xe,e as wt,i as Wt,o as Ut,a as Vt,r as Q,c as q,w as ce,F as xt,g as Ye,d as jt,f as Gt,T as $t}from"./index.331a7d37.js";/*!
   * shared v9.2.2
   * (c) 2022 kazuya kawaguchi
   * Released under the MIT License.
